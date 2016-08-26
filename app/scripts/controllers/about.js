402e24ede5c24c578489e3b33f421cd9bc3d66b0
Nick Taylor <taylorn@udel.edu>
2016-08-26 17:25:42 -0400

    Initial commit with scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherForeYouApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the weatherForeYouApp
+ */
+angular.module('weatherForeYouApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherForeYouApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the weatherForeYouApp
+ */
+angular.module('weatherForeYouApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
