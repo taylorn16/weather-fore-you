1f946e124d77d384bad54ed9925931560ce212b5
Nick Taylor <taylorn@udel.edu>
2016-08-28 00:15:39 -0400

    Improved comments in main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,19 +13,35 @@ angular.module('weatherForeYouApp')
   function (forecastService, $log, cityService, config, weatherCodeService, $scope) {
     var vm = this;
 
-    // Set up search models
+    // Set up search models...
     vm.searchQuery = '';
     vm.searchResults = [];
 
-    // Updating the search results happens each time the search input is changed
+    // ...and update the search results each time the search input is changed
     vm.updateSearchResults = function() {
-      cityService.getSearchResultsFor(vm.searchQuery)
-        .then(function(results) {
-          vm.searchResults = results;
-      });
+      if (vm.searchQuery) {
+        cityService.getSearchResultsFor(vm.searchQuery)
+          .then(function(results) {
+            vm.searchResults = results;
+        });
+      }
     };
 
-    // Apply 'fake' defaults to the view
+    // Function happens when a user clicks a search result from the city search api
+    vm.updateCurrentWeatherByResult = function(result) {
+      // Update city name & clear search results
+      vm.cityName = result.name;
+      vm.searchResults = [];
+      vm.searchQuery = result.name;
+
+      // Update forecast location in the view model
+      cityService.getLatAndLongById(result.id).then(function(location) {
+        vm.forecastParams.latitude = location.latitude;
+        vm.forecastParams.longitude = location.longitude;
+      });
+    }; // updateCurrentWeatherByResult()
+
+    // Initially apply 'fake' defaults to the view to avoid blank spaces...
     vm.cityName = config.cityDefaults.name;
     vm.weather = {
       temperature: '-',
@@ -38,15 +54,15 @@ angular.module('weatherForeYouApp')
     };
     vm.forecastParams = config.forecastParamDefaults;
 
-    // Initialize real weather values with defaults
+    // ... and load real weather values from default location (NY)
     forecastService.getCurrentWeather(vm.forecastParams)
       .then(function(weatherData) {
         vm.updateCurrentWeatherData(weatherData);
     });
 
     // Update all appropriate model values with appropriate modifications
+    // and parsings for units and for human-readable purposes
     vm.updateCurrentWeatherData = function(weatherData) {
-      $log.info('update weather data called');
       vm.weather = {
         temperature: weatherData.temperature,
         iconCode: weatherCodeService.getIconCode(weatherData.cloudCover),
@@ -58,19 +74,7 @@ angular.module('weatherForeYouApp')
       };
     };
 
-    // Function happens when a user clicks a search result from the city search api
-    vm.updateCurrentWeatherByResult = function(result) {
-      // Update city name
-      vm.cityName = result.name;
-
-      // Update forecast location in the view model
-      cityService.getLatAndLongById(result.id).then(function(location) {
-        vm.forecastParams.latitude = location.latitude;
-        vm.forecastParams.longitude = location.longitude;
-      });
-    }; // updateCurrentWeatherByResult()
-
-    // Set up a watch on the forecastParams and update current
+    // Set up a $watch on the forecastParams and update current
     // weather block as necessary
     $scope.$watch(
       function watchForecastParams(scope) {
